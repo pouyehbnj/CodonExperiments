2512ef7cbef293f36b277adbb4ec37641dedec8a
pouyehbnj <pouyehbnj@gmail.com>
2024-04-24 11:54:28 +0200

    subject prepration tests

--- Experiment-1/fannkuch/fannkuch.cpp
+++ Experiment-1/fannkuch/fannkuch.cpp
@@ -25,16 +25,16 @@ void perm(int n, long long i, std::vector<int>& p) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " n" << std::endl;
-        return 1;
-    }
+    // if (argc != 2) {
+    //     std::cerr << "Usage: " << argv[0] << " n" << std::endl;
+    //     return 1;
+    // }
 
     int n = std::atoi(argv[1]);
     std::vector<int> p(n);
     int max_flips = 0;
 
-    auto start_time = std::chrono::high_resolution_clock::now();
+    // auto start_time = std::chrono::high_resolution_clock::now();
     for (long long idx = 0; idx < factorial(n); idx++) {
         perm(n, idx, p);
         int flips = 0;
@@ -51,8 +51,8 @@ int main(int argc, char *argv[]) {
         }
         max_flips = std::max(flips, max_flips);
     }
-    auto end_time = std::chrono::high_resolution_clock::now();
-    double elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
+    // auto end_time = std::chrono::high_resolution_clock::now();
+    // double elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
 
     std::cout << "Pfannkuchen(" << n << ") = " << max_flips << std::endl;
     // std::cout << "Time taken: " << elapsed_time << " seconds" << std::endl;
