d401d7c78662438c4a553740e20dbf8070977c2b
pouyehbnj <pouyehbnj@gmail.com>
2024-04-24 13:15:57 +0200

    preparing binary_trees subject

--- Experiment-1/binary_trees/validation.sh
+++ Experiment-1/binary_trees/validation.sh
@@ -5,16 +5,11 @@ export BENCH_DIR=$(dirname "$0")
 export PYTHON="${EXE_PYTHON:-python3}"
 export CPP="${EXE_CPP:-clang++}"
 export CODON="${EXE_CODON:-build/codon}"
-export CSV_FILE="${BENCH_DIR}/execution_times_fixed_o3.csv"
+export CSV_FILE="${BENCH_DIR}/execution_times_fixed.csv"
 export LIZARD_CSV="${BENCH_DIR}/lizard_complexity.csv"
 
-# # # # # # # # # # # # # # # # # # # # # # 
-# Fix the seed for reproducibility
-RANDOM_SEED=12345
-RANDOM=$RANDOM_SEED
-
 # Prepare CSV file with header
-echo "run_number,execution_method,execution_time,tree_depth" > "${CSV_FILE}"
+echo "run_number,execution_method,execution_time,flag,SIZE" > "${CSV_FILE}"
 echo "Language,Total nloc,Avg.NLOC,AvgCCN,Avg.token,Fun Cnt,Warning cnt,Fun Rt,nloc Rt" > "${LIZARD_CSV}"
 
 # Lizard complexity analysis function
@@ -32,62 +27,80 @@ analyze_complexity() {
 # Run Lizard complexity analysis for each language
 analyze_complexity "${BENCH_DIR}/binary_trees.cpp" "C++"
 analyze_complexity "${BENCH_DIR}/binary_trees.py" "Python"
-analyze_complexity "${BENCH_DIR}/binary_trees.py" "Codon"
+analyze_complexity "${BENCH_DIR}/binary_trees_codon.py" "Codon"
 
 # Run tests for 10 random inputs
 for i in {1..10}
 do
     # Generate a random tree depth between 1 and 30
-    # TREE_DEPTH=$((1 + RANDOM % 20))
-    # echo "Running tests for tree depth: ${TREE_DEPTH}"
-    TREE_DEPTH=16
-    echo "Running tests for tree depth: ${TREE_DEPTH}"
+    # SIZE=$((1 + RANDOM % 10))
+    # echo "Running tests for tree depth: ${SIZE}"
+    SIZE=16
+    echo "Running tests for size: ${SIZE}"
 
-    # Compile C++ program
-    ${CPP} -std=c++17 -O3 "${BENCH_DIR}/binary_trees.cpp" -o "${BENCH_DIR}/binary_trees_cpp"
+    # Compile C++ program with o0
+    ${CPP} -std=c++17 -O0 "${BENCH_DIR}/binary_trees.cpp" -o "${BENCH_DIR}/binary_trees_cpp_o0"  
     
     # Run C++ program and measure time
-    START_TIME=$(python -c "import time; print(time.time())")
-    CPP_OUTPUT=$("${BENCH_DIR}/binary_trees_cpp" ${TREE_DEPTH})
-    END_TIME=$(python -c "import time; print(time.time())")
-    CPP_TIME=$(echo "$END_TIME - $START_TIME" | bc)
-    echo "C++ execution time: ${CPP_TIME}s"
-    echo "${i},cpp,${CPP_TIME},${TREE_DEPTH}" >> "${CSV_FILE}"
+    START_TIME=$(${PYTHON} -c "import time; print(time.time())")
+    CPP_OUTPUT=$("${BENCH_DIR}/binary_trees_cpp_o0" ${SIZE})
+    # echo "$CPP_OUTPUT"
+    END_TIME=$(${PYTHON} -c "import time; print(time.time())")
+    CPP_TIME_O0=$(echo "$END_TIME - $START_TIME" | bc)
+    echo "C++ execution time: ${CPP_TIME_O0}s"
+    echo "${i},cpp,${CPP_TIME_O0},o0,${SIZE}" >> "${CSV_FILE}"
+
+    # Compile C++ program with o3
+    ${CPP} -std=c++17 -O3 "${BENCH_DIR}/binary_trees.cpp" -o "${BENCH_DIR}/binary_trees_cpp_o3"
+    
+    # Run C++ program and measure time
+    START_TIME=$(${PYTHON} -c "import time; print(time.time())")
+    CPP_OUTPUT=$("${BENCH_DIR}/binary_trees_cpp_o3" ${SIZE})
+    # echo "$CPP_OUTPUT"
+    END_TIME=$(${PYTHON} -c "import time; print(time.time())")
+    CPP_TIME_O3=$(echo "$END_TIME - $START_TIME" | bc)
+    echo "C++ execution time: ${CPP_TIME_O3}s"
+    echo "${i},cpp,${CPP_TIME_O3},o3,${SIZE}" >> "${CSV_FILE}"
     
     # Run Python program and measure time
-    START_TIME=$(python -c "import time; print(time.time())")
-    PYTHON_OUTPUT=$(${PYTHON} "${BENCH_DIR}/binary_trees.py" ${TREE_DEPTH})
-    END_TIME=$(python -c "import time; print(time.time())")
+    START_TIME=$(${PYTHON} -c "import time; print(time.time())")
+    PYTHON_OUTPUT=$(${PYTHON} "${BENCH_DIR}/binary_trees.py" ${SIZE})
+    # echo "$PYTHON_OUTPUT"
+    END_TIME=$(${PYTHON} -c "import time; print(time.time())")
     PYTHON_TIME=$(echo "$END_TIME - $START_TIME" | bc)
     echo "Python execution time: ${PYTHON_TIME}s"
-    echo "${i},python,${PYTHON_TIME},${TREE_DEPTH}" >> "${CSV_FILE}"
+    echo "${i},python,${PYTHON_TIME},NA,${SIZE}" >> "${CSV_FILE}"
     
     # Compile Codon Python program
-    ${CODON} build --release "${BENCH_DIR}/binary_trees_codon.py"
+    ${CODON} build --release "${BENCH_DIR}/binary_trees_codon.py"  
     
     # Run Codon Python program and measure time
-    START_TIME=$(python -c "import time; print(time.time())")
-    CODON_OUTPUT=$("${BENCH_DIR}/binary_trees_codon" ${TREE_DEPTH})
-    END_TIME=$(python -c "import time; print(time.time())")
+    START_TIME=$(${PYTHON} -c "import time; print(time.time())")
+    CODON_OUTPUT=$("${BENCH_DIR}/binary_trees_codon" ${SIZE})
+    # echo "$CODON_OUTPUT"
+    END_TIME=$(${PYTHON} -c "import time; print(time.time())")
     CODON_TIME=$(echo "$END_TIME - $START_TIME" | bc)
-    echo "${i},codon,${CODON_TIME},${TREE_DEPTH}" >> "${CSV_FILE}"
+    echo "Codon execution time: ${CODON_TIME}s"
+    echo "${i},codon,${CODON_TIME},NA,${SIZE}" >> "${CSV_FILE}"
 
     # Compare outputs
     if [ "${CPP_OUTPUT}" == "${PYTHON_OUTPUT}" ] && [ "${PYTHON_OUTPUT}" == "${CODON_OUTPUT}" ]; then
-        echo "Test ${i}: All outputs match for depth ${TREE_DEPTH}."
+        echo "Test ${i}: All outputs match for depth ${SIZE}."
     else
-        echo "Test ${i}: Outputs do not match for depth ${TREE_DEPTH}."
+        echo "Test ${i}: Outputs do not match for depth ${SIZE}."
         echo "C++ output: ${CPP_OUTPUT}"
         echo "Python output: ${PYTHON_OUTPUT}"
         echo "Codon output: ${CODON_OUTPUT}"
     fi
-    rm "${BENCH_DIR}/binary_trees_cpp"
-    rm  "${BENCH_DIR}/binary_trees_codon"
-    cpp_times+=("$CPP_TIME")
+    rm "${BENCH_DIR}/binary_trees_cpp_o0"
+    rm "${BENCH_DIR}/binary_trees_cpp_o3"
+    rm "${BENCH_DIR}/binary_trees_codon"
+
+    cpp_times_O0+=("$CPP_TIME_O0")
+    cpp_times_O3+=("$CPP_TIME_O3")
     python_times+=("$PYTHON_TIME")
     codon_times+=("$CODON_TIME")
 done
-
 # Function to calculate mean using awk
 calculate_mean() {
     local times=("$@")
@@ -95,11 +108,13 @@ calculate_mean() {
 }
 
 # Calculate means
-cpp_mean=$(calculate_mean "${cpp_times[@]}")
+cpp_mean_O0=$(calculate_mean "${cpp_times_O0[@]}")
+cpp_mean_O3=$(calculate_mean "${cpp_times_O3[@]}")
 python_mean=$(calculate_mean "${python_times[@]}")
 codon_mean=$(calculate_mean "${codon_times[@]}")
 
 # Output means
-echo "C++ Mean Execution Time: $cpp_mean seconds"
-echo "Python Mean Execution Time: $python_mean seconds"
-echo "Codon Mean Execution Time: $codon_mean seconds"
+echo "C++ o0 Mean Execution Time: $cpp_mean_O0 seconds"  >> "${CSV_FILE}"
+echo "C++ 03 Mean Execution Time: $cpp_mean_O3 seconds"  >> "${CSV_FILE}"
+echo "Python Mean Execution Time: $python_mean seconds"  >> "${CSV_FILE}"
+echo "Codon Mean Execution Time: $codon_mean seconds"  >> "${CSV_FILE}"
