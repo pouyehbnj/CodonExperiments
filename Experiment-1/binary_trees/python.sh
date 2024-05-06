288b3f094e93b6f03d586148b2f599e5b3342e48
pouyehbnj <pouyehbnj@gmail.com>
2024-05-06 13:01:24 +0200

    just testing python 22

--- Experiment-1/binary_trees/python.sh
+++ Experiment-1/binary_trees/python.sh
@@ -24,11 +24,11 @@ log_process_stats() {
     # Start PowerJoular monitoring for the specific PID and capture its PID
     # local power_profile=powerjoular -p $pid -f "${BENCH_DIR}/power-python" 1> /dev/null
     sudo powerjoular -p $pid -f "${BENCH_DIR}/power-python" 1> /dev/null &
-    sleep 0.5
+    sleep 0.1
     local powerjoular_pid=$(pgrep -f "powerjoular -p $pid -f ${BENCH_DIR}/power-python")
     # local powerjoular_pid=$!
     # sleep 0.1
-    echo $powerjoular_pid
+    # echo $powerjoular_pid
     # # Check if PowerJoular started correctly
     # if ! kill -0 $powerjoular_pid 2> /dev/null; then
     #     echo "Failed to start PowerJoular for PID $pid"
