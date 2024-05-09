edc4651cf1a98e471ec514e7c98dc0a039b5d2ce
pouyehbnj <pouyehbnj@gmail.com>
2024-05-09 11:58:40 +0200

    added fannkuch 5

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -15,8 +15,8 @@ SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
 # Define the array of subjects (directories) with an array of sizes for each subject
 declare -A subject_sizes
 # subject_sizes["binary_trees"]="18 19 20"
-subject_sizes["fannkuch"]="10 11"
-# subject_sizes["fasta"]="10 20 30 40 50 60 70 80 90 100"
+subject_sizes["fannkuch"]="12 15 20"
+# subject_sizes["fasta"]="250000 "
 # subject_sizes["float"]="10 20 30 40 50 60 70 80 90 100"
 # subject_sizes["mandelbrot"]="10 20 30 40 50 60 70 80 90 100"
 # subject_sizes["nbody"]="10 20 30 40 50 60 70 80 90 100"
