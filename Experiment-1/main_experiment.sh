8c4ce4253275eef3dddabc13a6121e155e0da292
pouyehbnj <pouyehbnj@gmail.com>
2024-05-09 15:06:00 +0200

    subject nbody 2

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -19,7 +19,7 @@ declare -A subject_sizes
 # subject_sizes["fasta"]="20000000 30000000 10000000"
 # subject_sizes["float"]="40000000 30000000 20000000"
 # subject_sizes["mandelbrot"]="900 700 800"
-subject_sizes["nbody"]="800000 900000 1000000"
+subject_sizes["nbody"]="400000 300000 2000000"
 # subject_sizes["nsieve"]="10 20 30 40 50 60 70 80 90 100"
 # subject_sizes["primes"]="10 20 30 40 50 60 70 80 90 100"
 # subject_sizes["reverse_complement"]="10 20 30 40 50 60 70 80 90 100"
