287037c0dd829a7689b1b3e9dcefa7ac616c3433
pouyehbnj <pouyehbnj@gmail.com>
2024-05-10 15:46:25 +0200

    subject reverse complement sizes 11

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -22,7 +22,7 @@ declare -A subject_sizes
 # subject_sizes["nbody"]="4000000 5000000 4500000"
 # subject_sizes["nsieve"]="13 14 15"
 # subject_sizes["primes"]="2006000 2008000 2007000" #to be decideddd!
-subject_sizes["reverse_complement"]="990000 980000 985000"
+subject_sizes["reverse_complement"]="1000000 2000000 3000000"
 # subject_sizes["set_partition"]="10 20 30 40 50 60 70 80 90 100"
 # subject_sizes["spectral_norm"]="10 20 30 40 50 60 70 80 90 100"
 # subject_sizes["taq"]="10 20 30 40 50 60 70 80 90 100"
