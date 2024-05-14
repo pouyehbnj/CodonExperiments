ec5c3dd5b4240ebdfe85beba4968f7b096866fba
pouyehbnj <pouyehbnj@gmail.com>
2024-05-14 13:23:01 +0200

    redoing taq subject experiments small trial 3

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -25,7 +25,7 @@ declare -A subject_sizes
 # subject_sizes["reverse_complement"]="35000000" #to be decideddd!
 # subject_sizes["set_partition"]="16 14 15"
 # subject_sizes["spectral_norm"]="400 300 500"
-subject_sizes["taq"]="10 50 100"
+subject_sizes["taq"]="100 1000 10000"
 
 # Create an array from the keys of the associative array for random selection
 subjects=(${!subject_sizes[@]})
