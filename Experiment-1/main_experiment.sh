1365b9e5a2c51fb5994cc6469b93a5117fbd2727
pouyehbnj <pouyehbnj@gmail.com>
2024-05-23 15:33:19 +0200

    testing nsieve 5

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -31,8 +31,8 @@ declare -A size_values
 # size_values["set_partition_medium"]="16 17"
 # size_values["set_partition_large"]="18 19"
 
-size_values["nsieve_small"]="14 15 13"
-# size_values["nsieve_medium"]="12 13"
+# size_values["nsieve_small"]="14 15 13"
+size_values["nsieve_medium"]="16 17 18"
 # size_values["nsieve_large"]="14 15"
 
 # size_values["fasta_small"]="20000000 25000000 17000000"
@@ -69,7 +69,7 @@ methods=("cpp" "python" "codon")
 # methods=("cpp")
 # Define the array of size categories
 # size_categories=("small" "medium" "large")
-size_categories=("small")
+size_categories=("medium")
 
 # Associative array to track execution counts
 declare -A execution_counts
