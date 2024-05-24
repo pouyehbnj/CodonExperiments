cc21e556a3605a7944636d02727b2c5aab511cc0
pouyehbnj <pouyehbnj@gmail.com>
2024-05-24 18:21:00 +0200

    rerun nsieve

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -31,9 +31,9 @@ declare -A size_values
 # size_values["set_partition_medium"]="16 17"
 # size_values["set_partition_large"]="18 19"
 
-# size_values["nsieve_small"]="14 15 13"
-# size_values["nsieve_medium"]="16 17 18"
-# size_values["nsieve_large"]="19 20 21"
+size_values["nsieve_small"]="14 15 13"
+size_values["nsieve_medium"]="16 17 18"
+size_values["nsieve_large"]="19 20 21"
 
 # size_values["fasta_small"]="20000000 25000000 17000000"
 # size_values["fasta_medium"]="31000000 36000000 38000000"
