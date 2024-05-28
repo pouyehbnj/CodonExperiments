152f7d8555820a1380c458691b51fa69b7fbf7c3
pouyehbnj <pouyehbnj@gmail.com>
2024-05-28 15:41:40 +0200

    testing nsieve 9

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -23,6 +23,10 @@ SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
 # size_values["primes_large"]="6600000 6700000 6800000"
 
 declare -A size_values
+# size_values["reverse_complement_small"]="10000000 15000000 20000000"
+# size_values["reverse_complement_medium"]="25000000 30000000 35000000"
+# size_values["reverse_complement_large"]="40000000 45000000 50000000"
+
 # size_values["binary_trees_small"]="18 17" # under 1 minute
 # size_values["binary_trees_medium"]="19" # 1-4 minutes
 # size_values["binary_trees_large"]="20 21" # 4-5 minutes
@@ -31,9 +35,9 @@ declare -A size_values
 # size_values["set_partition_medium"]="15"
 # size_values["set_partition_large"]="16"
 
-# size_values["nsieve_small"]="14 13"
+size_values["nsieve_small"]="12 13"
 # size_values["nsieve_medium"]="16 15"
-size_values["nsieve_large"]="17 18"
+# size_values["nsieve_large"]="17 18"
 
 # size_values["fasta_small"]="20000000 25000000 17000000"
 # size_values["fasta_medium"]="31000000 36000000 38000000"
@@ -66,10 +70,10 @@ echo "All subjects: ${subjects[@]}"
 # Define the array of methods
 # methods=("cpp" "python" "codon")
 # Define the array of methods
-methods=("python")
+methods=("cpp")
 # Define the array of size categories
 # size_categories=("small" "medium" "large")
-size_categories=("large")
+size_categories=("small")
 
 # Associative array to track execution counts
 declare -A execution_counts
