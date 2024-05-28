53889fae2248f3d377d184abc9fed737f0641c36
pouyehbnj <pouyehbnj@gmail.com>
2024-05-28 15:04:47 +0200

    testing nsieve 6

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -69,7 +69,7 @@ echo "All subjects: ${subjects[@]}"
 methods=("cpp" "codon")
 # Define the array of size categories
 # size_categories=("small" "medium" "large")
-size_categories=("large")
+size_categories=("small")
 
 # Associative array to track execution counts
 declare -A execution_counts
