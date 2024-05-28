af6dc4e90d58b0cc7e3b65d28cfab416c65ae3d6
pouyehbnj <pouyehbnj@gmail.com>
2024-05-28 22:02:44 +0200

    testing binary trees 3

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -27,9 +27,9 @@ declare -A size_values
 # size_values["reverse_complement_medium"]="25000000 30000000 35000000"
 # size_values["reverse_complement_large"]="40000000 45000000 50000000"
 
-size_values["binary_trees_small"]="19 18" # under 1 minute
-# size_values["binary_trees_medium"]="19 28" # 1-4 minutes
-# size_values["binary_trees_large"]="20 21" # 4-5 minutes
+# size_values["binary_trees_small"]="19 18" # under 1 minute
+# size_values["binary_trees_medium"]="20 21" # 1-4 minutes
+size_values["binary_trees_large"]="23 22" # 4-5 minutes
 
 # size_values["set_partition_small"]="14"
 # size_values["set_partition_medium"]="15"
@@ -70,10 +70,10 @@ echo "All subjects: ${subjects[@]}"
 # Define the array of methods
 # methods=("cpp" "python" "codon")
 # Define the array of methods
-methods=("cpp")
+methods=("python")
 # Define the array of size categories
 # size_categories=("small" "medium" "large")
-size_categories=("small")
+size_categories=("large")
 
 # Associative array to track execution counts
 declare -A execution_counts
