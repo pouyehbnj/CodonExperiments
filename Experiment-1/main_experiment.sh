a86bda1e9c0c3bb08bb7dadf1d660bca0583f83d
pouyehbnj <pouyehbnj@gmail.com>
2024-05-29 14:58:57 +0200

    checking reverse complement

--- Experiment-1/main_experiment.sh
+++ Experiment-1/main_experiment.sh
@@ -23,13 +23,14 @@ SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
 # size_values["primes_large"]="6600000 6700000 6800000"
 
 declare -A size_values
-# size_values["reverse_complement_small"]="10000000 15000000 20000000"
+
+size_values["reverse_complement_small"]="10000000 15000000 20000000"
 # size_values["reverse_complement_medium"]="25000000 30000000 35000000"
 # size_values["reverse_complement_large"]="40000000 45000000 50000000"
 
-size_values["binary_trees_small"]="18" # under 1 minute
-size_values["binary_trees_medium"]="19" # 1-4 minutes
-size_values["binary_trees_large"]="20" # 4-5 minutes
+# size_values["binary_trees_small"]="18" 
+# size_values["binary_trees_medium"]="19" 
+# size_values["binary_trees_large"]="20" 
 
 # size_values["set_partition_small"]="14"
 # size_values["set_partition_medium"]="15"
